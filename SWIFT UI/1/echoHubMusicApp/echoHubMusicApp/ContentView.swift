38c492ffc91ee39ff6c4e1ec35d2b7ba8b3c486a
Your Name <you@example.com>
2025-07-25 04:43:10 +0530

    1. First commit

---
+++ SWIFT UI/1/echoHubMusicApp/echoHubMusicApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echoHubMusicApp
+//
+//  Created by alens-caw-mac-m1 on 25/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWIFT UI/1/echoHubMusicApp/echoHubMusicApp/echoHubMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoHubMusicAppApp.swift
+//  echoHubMusicApp
+//
+//  Created by alens-caw-mac-m1 on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct echoHubMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
