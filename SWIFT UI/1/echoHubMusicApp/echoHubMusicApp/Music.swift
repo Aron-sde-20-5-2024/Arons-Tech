8547b27ff8cccdf6563358c43fda5fbfc55d7f43
Aron-sde-20-5-2024 <arontvs2@gmail.com>
2025-07-27 06:05:38 +0530

    removed Music Class from content.swift and added at Music.swift

---
+++ SWIFT UI/1/echoHubMusicApp/echoHubMusicApp/Music.swift
@@ -0,0 +1,15 @@
+import Fountation
+
+class Music {
+    init (id: Int, title: String, singer: String, image: String) {
+        self.id = id
+        self.title = title
+        self.singer = singer
+        self.image = image
+    }
+    
+    let id: Int
+    let title: String
+    let singer: String
+    let image: String
+}
